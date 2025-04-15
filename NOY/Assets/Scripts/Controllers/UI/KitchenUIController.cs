b46282fea574e51bb9fa11e1c4e97610249f808d
Halcy0nin <155969605+Halcy0nin@users.noreply.github.com>
2025-04-15 11:04:56 +0800

    basic ui elements implementation

--- NOY/Assets/Scripts/Controllers/UI/KitchenUIController.cs
+++ NOY/Assets/Scripts/Controllers/UI/KitchenUIController.cs
@@ -1,7 +1,12 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class KitchenUIController : MonoBehaviour
 {
+
+    public Sprite[] healthyFoodImages;
+    public Sprite[] junkFoodImages;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
