f574d3fcc09478ce622cb649562df3378d447db7
Halcy0nin <155969605+Halcy0nin@users.noreply.github.com>
2025-04-17 10:44:31 +0800

    Refridgerator ui

--- NOY/Assets/Scripts/Controllers/UI/BedUIController.cs
+++ NOY/Assets/Scripts/Controllers/UI/BedUIController.cs
@@ -5,25 +5,24 @@ public class BedUIController : MonoBehaviour
 {
     public Sprite[] sleepingCharacter; // 0 = sleeping, 1 = awake
     public Image characterImage;       // The UI Image to update
-    public bool sleeping;
 
     public NeedsController NeedsController;
 
     public void ToggleSleep(bool sleeping)
     {
-        if (sleeping && NeedsController.petGender == "M")
+        if (NeedsController.Sleeping && NeedsController.petGender == "M")
         {
             characterImage.sprite = sleepingCharacter[0];
         }
-        else if (!sleeping && NeedsController.petGender == "M")
+        else if (!NeedsController.Sleeping && NeedsController.petGender == "M")
         {
             characterImage.sprite = sleepingCharacter[1];
         }
-        else if (sleeping && NeedsController.petGender == "F")
+        else if (NeedsController.Sleeping && NeedsController.petGender == "F")
         {
             characterImage.sprite = sleepingCharacter[2];
         }
-        else if (!sleeping && NeedsController.petGender == "F")
+        else if (!NeedsController.Sleeping && NeedsController.petGender == "F")
         {
             characterImage.sprite = sleepingCharacter[3];
         }
